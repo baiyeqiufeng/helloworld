184fc457cdd6f7752133d70517ce004c68371cad
baiyeqiufeng <2101978225@qq.com>
2019-03-11 17:55:13 +0800

    提交代码

---
+++ src/Test/Test.java
@@ -0,0 +1,24 @@
+package Test;
+
+public class Test{
+    public static Test aTest = null;    
+    public Test(){
+        System.out.println(aTest);
+        aTest = this;
+    }
+    
+    public static Test getInstance() {
+        if(aTest == null) {
+            new Test();
+        }
+        return aTest;
+    }
+    
+    public static void main(String []args) {
+        System.out.println(getInstance().toString());
+        System.out.println(getInstance());
+//        Test aTest = new Test();
+//        System.out.println(aTest.toString());
+//        System.out.println(new Test().toString());
+    }
+}
